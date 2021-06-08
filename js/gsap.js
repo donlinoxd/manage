ba75471ce97852db493e78abccd868f075017b1b
don.lino.xd <don.lino.xd@gmail.com>
2021-06-09 01:03:05 +0800

    commit

--- js/gsap.js
+++ js/gsap.js
@@ -1,86 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
-  gsap.fromTo(
-    ".anim-down",
-    {
-      opacity: 0,
-      y: -50,
-    },
-    {
-      duration: 2,
-      opacity: 2,
-      y: 0,
-      stagger: 0.2,
-      ease: "back",
-    }
-  );
+  gsap.from(".anim-down", {
+    opacity: 0,
+    y: -50,
+    duration: 2,
+    stagger: 0.2,
+    ease: "back",
+  });
 
-  gsap.fromTo(
-    ".anim-up",
-    {
-      opacity: 0,
-      y: 100,
-    },
-    {
-      opacity: 1,
-      duration: 2,
-      delay: 1,
-      y: 0,
-      stagger: 0.4,
-      ease: "back",
-    }
-  );
+  gsap.from(".anim-up", {
+    opacity: 0,
+    y: 100,
+    duration: 2,
+    delay: 1,
+    stagger: 0.4,
+    ease: "power2",
+  });
 
-  gsap.fromTo(
-    ".anim-right",
-    {
-      opacity: 0,
-      x: 100,
-    },
-    {
-      opacity: 1,
-      x: 0,
-      delay: 2,
-      duration: 2,
-      ease: "back",
-    }
-  );
+  gsap.from(".anim-scale-out", {
+    opacity: 0,
+    duration: 1.5,
+    delay: 2,
+    scale: 0.2,
+    transformOrigin: "center",
+    ease: "back",
+  });
 
-  gsap.fromTo(
-    ".scroll-anim-right",
-    {
-      x: 500,
-      opacity: 0,
-    },
-    {
-      scrollTrigger: {
-        trigger: ".scroll-anim-right",
-        start: "top center",
-        end: "+=500",
-        once: true,
-      },
-      opacity: 1,
-      stagger: 0.5,
-      x: 0,
-      ease: "power2",
-    }
-  );
+  gsap.from(".anim-scale-y-top", {
+    opacity: 0,
+    duration: 1,
+    delay: 3.5,
+    stagger: 0.5,
+    scaleY: 0.2,
+    transformOrigin: "top",
+    ease: "power2",
+  });
 
-  gsap.fromTo(
-    ".scroll-anim-left",
-    {
-      opacity: 0,
-    },
-    {
-      scrollTrigger: {
-        trigger: ".scroll-anim-left",
-        start: "top center",
-        end: "+=500",
-        once: true,
-      },
-      opacity: 1,
-      stagger: 0.7,
-      x: 0,
-      ease: "back",
-    }
-  );
+  gsap.from(".anim-scale-y-bot", {
+    opacity: 0,
+    duration: 1,
+    delay: 4.5,
+    scaleY: 0.2,
+    stagger: 0.3,
+    transformOrigin: "bottom",
+    ease: "back",
+  });
+
+  gsap.from(".anim-slide-up", {
+    opacity: 0,
+    delay: 4.75,
+    stagger: 0.25,
+    duration: 2,
+    ease: "power2",
+    translateY: 100,
+  });
+
+  gsap.from(".anim-rotation", {
+    opacity: 0,
+    duration: 3.5,
+    delay: 5,
+    ease: "slow",
+    rotation: 360,
+    transformOrigin: "center",
+  });
 });
